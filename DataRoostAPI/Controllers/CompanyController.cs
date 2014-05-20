6a5eebbefc8c0b123731e06dd049f853d2d02d18
Kevin Barlett <kbarlett@factset.com>
2014-05-20 09:12:10 -0400

    API: Introduce non-product specific infrastructure

---
+++ DataRoostAPI/Controllers/CompanyController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CCS.Fundamentals.DataRoostAPI.Models;
+
+namespace CCS.Fundamentals.DataRoostAPI.Controllers {
+
+	[RoutePrefix("api/v1")]
+	public class CompanyController : ApiController {
+
+		[Route("companies/")]
+		[HttpGet]
+		public CompanyDTO[] QueryCompanies() {
+			throw new NotImplementedException();
+		}
+
+		[Route("companies/{CompanyId}")]
+		[HttpGet]
+		public CompanyDTO[] GetCompanies(string CompanyId) {
+			throw new NotImplementedException();
+		}
+
+		[Route("companies/{CompanyId}/efforts/")]
+		[HttpGet]
+		public EffortDTO[] QueryEfforts(string CompanyId) {
+			throw new NotImplementedException();
+		}
+	}
+}
