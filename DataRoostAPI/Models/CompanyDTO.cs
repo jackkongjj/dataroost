71ff3beb6f9e465d2a1e9e8a002132773836771d
Isaac Blatter <iblatter@factset.com>
2014-07-02 11:01:23 -0400

    API: Added company info to dataroost
    Added Company DTO objects describing the API for /company
    Implementation for /company/{permId} pulling the data from ffDocHist

--- DataRoostAPI/Models/CompanyDTO.cs
+++ DataRoostAPI/Models/CompanyDTO.cs
@@ -2,8 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Newtonsoft.Json;
 
 namespace CCS.Fundamentals.DataRoostAPI.Models {
 	public class CompanyDTO {
+
+		[JsonProperty(PropertyName = "_id")]
+		public string Id { get; set; }
+
+		[JsonProperty(PropertyName = "rootPPI")]
+		public string RootPPI { get; set; }
+
+		[JsonProperty(PropertyName = "entityPermId")]
+		public string EntitiyPermId { get; set; }
+
+		[JsonProperty(PropertyName = "iconum")]
+		public int Iconum { get; set; }
+
+		[JsonProperty(PropertyName = "name")]
+		public string Name { get; set; }
+
+		[JsonProperty(PropertyName = "companyTyype")]
+		public string CompanyType { get; set; }
+
+		[JsonProperty(PropertyName = "countryId")]
+		public string CountryId { get; set; }
+
+		[JsonProperty(PropertyName = "country")]
+		public CountryDTO Country { get; set; }
+
+		[JsonProperty(PropertyName = "primaryShareClassId")]
+		public string PrimaryShareClassId { get; set; }
+
+		[JsonProperty(PropertyName = "shareClasses")]
+		public IEnumerable<ShareClassDTO> ShareClasses { get; set; }
 	}
 }
