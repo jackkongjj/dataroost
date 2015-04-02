aaeae5d82c2c994a96d310dc8c6bf0b8c4d7b7f4
Isaac Blatter <iblatter@factset.com>
2015-04-02 10:59:31 -0400

    Added status and version services and exception handler

---
+++ DataRoostAPI/Controllers/InfoController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Reflection;
+using System.Web.Http;
+
+using CCS.Fundamentals.DataRoostAPI.Filters;
+
+namespace CCS.Fundamentals.DataRoostAPI.Controllers
+{
+	[ExceptionHandlerFilter]
+	[RoutePrefix("api/v1")]
+	public class StatusController : ApiController {
+
+		[AllowAnonymous]
+		[Route("status/")]
+		[HttpGet]
+		public string GetStatus() {
+			return "SUCCESS";
+		}
+
+		[AllowAnonymous]
+		[Route("version/")]
+		[HttpGet]
+		public string GetVersion() {
+			return Assembly.GetExecutingAssembly().GetName().Version.ToString();
+		}
+	}
+}
---
+++ DataRoostAPI/Filters/ExceptionHandlerFilter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http.Filters;
+
+using log4net;
+
+namespace CCS.Fundamentals.DataRoostAPI.Filters {
+
+	public class ExceptionHandlerFilter : ExceptionFilterAttribute {
+
+		private static readonly ILog logger = LogManager.GetLogger(typeof(ExceptionHandlerFilter));
+
+		public override void OnException(HttpActionExecutedContext actionExecutedContext) {
+			string message = string.Format("URL: {0}", actionExecutedContext.Request.RequestUri);
+			logger.Error(message, actionExecutedContext.Exception);
+		}
+	}
+}
