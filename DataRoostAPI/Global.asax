3ecb2b0f28159ba22c937b540b2e0fd3d255d0ae
Isaac Blatter <iblatter@factset.com>
2015-04-23 15:56:16 -0400

    Updating DataRoost to use OWIN hosting

--- DataRoostAPI/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CCS.Fundamentals.DataRoostAPI.WebApiApplication" Language="C#" %>
---
+++ DataRoostAPI/Startup.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Web;
+using System.Web.Http;
+
+using CCS.Fundamentals.DataRoostAPI.Filters;
+
+using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+
+using Owin;
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+[assembly: OwinStartup(typeof(DataRoost.Startup))]
+namespace DataRoost {
+
+	public class Startup {
+
+		public void Configuration(IAppBuilder app) {
+
+			app.UseCors(CorsOptions.AllowAll);
+
+			HttpConfiguration config = new HttpConfiguration();
+
+			config.Formatters.Clear();
+			JsonMediaTypeFormatter jsonFormatter = new JsonMediaTypeFormatter();
+			var enumConverter = new Newtonsoft.Json.Converters.StringEnumConverter();
+			jsonFormatter.SerializerSettings.Converters.Add(enumConverter);
+			config.Formatters.Add(jsonFormatter);
+
+			config.Filters.Add(new ExceptionHandlerFilter());
+
+			config.MapHttpAttributeRoutes();
+
+			app.UseWebApi(config);
+		}
+	}
+}
