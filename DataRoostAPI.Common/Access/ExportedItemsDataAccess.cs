797f2507c9ca62053ac89c02aa46393469ff3620
Isaac Blatter <iblatter@factset.com>
2015-06-04 15:57:20 -0400

    Added missing items

---
+++ DataRoostAPI.Common/Access/ExportedItemsDataAccess.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+using DataRoostAPI.Common.Interfaces;
+using DataRoostAPI.Common.Models;
+
+using Fundamentals.Helpers.DataAccess;
+
+namespace DataRoostAPI.Common.Access {
+	internal class ExportedItemsDataAccess : ApiHelper, IExportedItemsDataAccess {
+
+		private readonly string _dataRoostConnectionString;
+
+		public ExportedItemsDataAccess(string dataRoostConnectionString) {
+			_dataRoostConnectionString = dataRoostConnectionString;
+		}
+
+		public ExportedItem[] GetExportedItems(StandardizationType standardizationType,
+																			 List<string> itemCodes,
+																			 DateTime startDate,
+																			 DateTime? endDate = null) {
+			string requestUrl = string.Format("{0}/api/v1/exportedItems/{1}?itemCodes={2}&startDate={3}", _dataRoostConnectionString, standardizationType, string.Join(",", itemCodes), startDate);
+			if (endDate != null) {
+				requestUrl += "&endDate=" + endDate;
+			}
+			return ExecuteGetQuery<ExportedItem[]>(requestUrl);
+		}
+
+
+		protected override WebClient GetDefaultWebClient() {
+			WebClient defaultClient = new WebClient();
+
+			defaultClient.Credentials = CredentialCache.DefaultNetworkCredentials;
+			defaultClient.Encoding = Encoding.UTF8;
+
+			return defaultClient;
+		}
+	}
+}
---
+++ DataRoostAPI/Access/SuperFast/ExportedItemsHelper.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+
+using DataRoostAPI.Common.Models;
+
+using FactSet.Data.SqlClient;
+
+using Fundamentals.Helpers.DataAccess;
+
+namespace CCS.Fundamentals.DataRoostAPI.Access.SuperFast {
+
+	public class ExportedItemsHelper : SqlHelper {
+
+		private readonly string _sfConnectionString;
+
+		public ExportedItemsHelper(string sfConnectionString) {
+			_sfConnectionString = sfConnectionString;
+		}
+
+		public ExportedItem[] GetExportedItems(StandardizationType standardizationType,
+		                                       List<string> itemCodes,
+		                                       DateTime startDate,
+		                                       DateTime endDate) {
+			const string sdbQuery = @"select DISTINCT ds.CompanyID, d.DocumentDate, d.FormTypeID, d.PublicationDateTime, d.DAMDocumentId
+																	from Document d (nolock)
+																			join DocumentSeries ds (nolock)
+																					on d.DocumentSeriesID = ds.ID
+																			join ExportedDocumentLog edl (nolock)
+																					on edl.DocumentID = d.DAMDocumentId
+																					and edl.CompanyID = ds.CompanyID
+																			join TimeSeries ts 
+																					on d.Id = ts.DocumentId
+																					and ts.EncoreFlag = 0
+																					and ts.AutoCalcFlag = 0
+																			join SDBTimeSeriesDetailSecurity sdbs (nolock)
+																					on ts.ID = sdbs.TimeSeriesID
+																			join SDBItem s (nolock)
+																					on sdbs.SdbItemId = s.id
+																			join fnc_SplitString(@itemCodes, ',') as fnc
+																					on s.SDBCode = fnc.token        
+																	where EndTimeStamp between @StartTime and @EndTime
+																	order by s.Id, d.id, ts.TimeSeriesDate desc";
+
+			const string stdQuery = @"select DISTINCT ds.CompanyID, d.DocumentDate, d.FormTypeID, d.PublicationDateTime, d.DAMDocumentId
+																	from Document d (nolock)
+																			join DocumentSeries ds (nolock)
+																					on d.DocumentSeriesID = ds.ID
+																			join ExportedDocumentLog edl (nolock)
+																					on edl.DocumentID = d.DAMDocumentId
+																					and edl.CompanyID = ds.CompanyID
+																			join TimeSeries ts 
+																					on d.Id = ts.DocumentId
+																					and ts.EncoreFlag = 0
+																					and ts.AutoCalcFlag = 0
+																			join STDTimeSeriesDetailSecurity stds (nolock)
+																					on ts.ID = stds.TimeSeriesID
+																			join STDItem s (nolock)
+																					on stds.STDItemId = s.id     
+																			join fnc_SplitString(@itemCodes, ',') as fnc
+																					on s.STDCode = fnc.token        
+																	where EndTimeStamp between @startDate and @endDate
+																	order by ds.CompanyID, d.DocumentDate, d.FormTypeID, d.PublicationDateTime";
+			string query = stdQuery;
+			if (standardizationType == StandardizationType.SDB) {
+				query = sdbQuery;
+			}
+
+			string itemCodeString = string.Join(",", itemCodes);
+
+			return
+				ExecuteQuery(query,
+				             new List<SqlParameter>
+				             {
+					             new SqlParameter("@startDate", startDate),
+					             new SqlParameter("@endDate", endDate),
+					             new SqlParameter("@itemCodes", itemCodeString)
+				             },
+				             reader => {
+					             return new ExportedItem
+					                    {
+						                    Iconum = reader.GetInt32(0).ToString(),
+						                    ReportDate = reader.GetDateTime(1),
+						                    FormType = reader.GetStringSafe(2),
+						                    PublicationDate = reader.GetDateTime(3),
+																DocumentId = reader.GetGuid(4).ToString()
+					                    };
+				             }).ToArray();
+		}
+
+		protected override SqlConnection GetDatabaseConnection() {
+			return new SqlConnection(_sfConnectionString);
+		}
+
+	}
+
+}
