4c6554f3feb4e12e8c3336f06beb86ec2272ac1a
gdigumarthy <gdigumarthy@factset.com>
2016-12-22 10:44:24 -0500

    breakout

--- DataRoostAPI.Common/Models/TimeseriesValues/SegmentNode.cs
+++ DataRoostAPI.Common/Models/TimeseriesValues/SegmentNode.cs
@@ -25,6 +25,8 @@ namespace DataRoostAPI.Common.Models.TimeseriesValues {
 		public string Type { get; set; }
 		[JsonIgnore]
 		public int? SegmentId { get; set; }
+		[JsonIgnore]
+		public bool IsBreakOut { get; set; }
 	}
 
 	public class FootNotes {
