cdcb18f1a52c8f6b1199de6de2f495713e1923a9
Lun De Jiang <ljiang@factset.com>
2017-03-24 02:44:46 -0400

    thinking about adding a Factory Method pattern to help converting result.  The idea is to pass Template name into reflection to get type, and then pass the type to Generic function that returns the result

--- DataRoostAPI.Common/Models/AsReported/AsReportedTemplate.cs
+++ DataRoostAPI.Common/Models/AsReported/AsReportedTemplate.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataRoostAPI.Common.Interfaces;
 
 namespace DataRoostAPI.Common.Models.AsReported {
 	public class AsReportedTemplate {
 		public List<StaticHierarchy> StaticHierarchies { get; set; }
 		public List<TimeSlice> TimeSlices { get; set; }
+
+        public string GetTemplateType()
+        {
+            return "AsReportedTemplate";
+        }
 	}
 }
