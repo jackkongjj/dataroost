0ab9cede30c2dd108c9377baf72940ff1878bfdd
Adam Pitzer <apitzer@factset.com>
2018-02-06 14:29:42 -0600

    Fix to also find LPV from certain Hierarchy Metas"

--- DataRoostAPI/Controllers/AsReportedController.cs
+++ DataRoostAPI/Controllers/AsReportedController.cs
@@ -569,6 +569,7 @@ namespace CCS.Fundamentals.DataRoostAPI.Controllers {
 			return result;
 		}
 
+		private List<string> LPVMetaTypes = new List<string>() { "NI", "RV", "TA", "TL", "LE", "PS", "PE", "CC" };
 
 		[Route("documents/{damdocumentId}/mtmwandlpv2")]
 		[HttpPut]
@@ -587,7 +588,7 @@ namespace CCS.Fundamentals.DataRoostAPI.Controllers {
 			//IEnumerable<StaticHierarchy> shs = templates.SelectMany(t => t.StaticHierarchies.Where(sh => sh.Cells.Any(c => c.LikePeriodValidationFlag || c.MTMWValidationFlag)));
 			IEnumerable<SCARAPITableCell> cells = templates.SelectMany(t => t.StaticHierarchies.SelectMany(sh => sh.Cells.Where(c => (c.MTMWValidationFlag || 
 					(c.LikePeriodValidationFlag
-					&& (sh.UnitTypeId == 2 || sh.UnitTypeId == 1)
+					&& (sh.UnitTypeId == 2 || sh.UnitTypeId == 1 || LPVMetaTypes.Contains(sh.StaticHierarchyMetaType))
 					&& templates.First(te => te.TimeSlices.Any(ts => ts.Cells.Contains(c))).TimeSlices.First(ti => ti.Cells.Contains(c)).DamDocumentId == damdocumentId
 					)
 				))));
@@ -640,7 +641,7 @@ namespace CCS.Fundamentals.DataRoostAPI.Controllers {
 					isSuccess = false;
 				}
 
-				IEnumerable<SCARAPITableCell> lpvcells = templates.SelectMany(t => t.StaticHierarchies.Where(sh => sh.UnitTypeId == 2 || sh.UnitTypeId == 1)
+				IEnumerable<SCARAPITableCell> lpvcells = templates.SelectMany(t => t.StaticHierarchies.Where(sh => sh.UnitTypeId == 2 || sh.UnitTypeId == 1 || LPVMetaTypes.Contains(sh.StaticHierarchyMetaType))
 					.SelectMany(sh => sh.Cells.Where(c => c.LikePeriodValidationFlag
 					&& templates.First().TimeSlices.First(ti => ti.Cells.Contains(c)).DamDocumentId == damdocumentId)));
 				int lpvcount = lpvcells.Count();
