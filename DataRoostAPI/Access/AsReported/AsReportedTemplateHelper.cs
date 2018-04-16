67ddbc76c17f4e2c8cc0de043c2c557ad3ff31e1
Lun De Jiang <ljiang@factset.com>
2018-04-16 10:19:25 -0400

    committing TDP save if no error

--- DataRoostAPI/Access/AsReported/AsReportedTemplateHelper.cs
+++ DataRoostAPI/Access/AsReported/AsReportedTemplateHelper.cs
@@ -3280,7 +3280,7 @@ OUTPUT $action, 'DimensionToCell', inserted.TableCellID INTO @ChangeResult;
 				sb.AppendLine("select @totalInsert = count(*) from @ChangeResult where ChangeType = 'INSERT';");
 				sb.AppendLine("select @totalUpdate = count(*) from @ChangeResult where ChangeType = 'UPDATE'; ");
 				sb.AppendLine();
-				sb.AppendLine(string.Format("IF (@totalInsert = {0} and @totalUpdate = {1}) BEGIN select 'commit'; ROLLBACK TRAN END ELSE BEGIN select 'rollback'; ROLLBACK TRAN END", totalInsert, totalUpdates));
+				sb.AppendLine(string.Format("IF (@totalInsert = {0} and @totalUpdate = {1}) BEGIN select 'commit'; COMMIT TRAN END ELSE BEGIN select 'rollback'; ROLLBACK TRAN END", totalInsert, totalUpdates));
 				result.ReturnValue["DebugMessage"] += sb.ToString();
 
 //				return result;
