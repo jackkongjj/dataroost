3b6d27d6f395ad293e26d6d66d83854970d1a6ad
Lun De Jiang <ljiang@factset.com>
2018-06-28 00:01:51 -0400

    prevent TableDimension from changing DocumentTableId

--- DataRoostAPI/Access/AsReported/AsReportedTemplateHelper.cs
+++ DataRoostAPI/Access/AsReported/AsReportedTemplateHelper.cs
@@ -3612,7 +3612,7 @@ DELETE FROM TableDimension where id in ({0});
 USING (VALUES ({0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9})) as src ( ID  ,DocumentTableID  ,DimensionTypeID  ,Label  ,OrigLabel  ,Location  ,EndLocation  ,Parent  ,InsertedRow  ,AdjustedOrder)
 ON TableDimension.id = src.ID
 WHEN MATCHED THEN
-	UPDATE SET       DocumentTableID  = src.DocumentTableID 
+	UPDATE SET       DocumentTableID  = TableDimension.DocumentTableID 
       ,DimensionTypeID  = src.DimensionTypeID 
       ,Label  = src.Label 
       ,OrigLabel  = src.OrigLabel 
