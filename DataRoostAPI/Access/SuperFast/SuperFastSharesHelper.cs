f15f2343a245562986e67672d15f0a20a36c7857
Lun De Jiang <ljiang@factset.com>
2018-07-17 10:32:32 -0400

    Revert "Shares"
    This reverts commit 70ad5f42dd24d31edd7425d12332cf527babe793.

--- DataRoostAPI/Access/SuperFast/SuperFastSharesHelper.cs
+++ DataRoostAPI/Access/SuperFast/SuperFastSharesHelper.cs
@@ -22,29 +22,9 @@ namespace CCS.Fundamentals.DataRoostAPI.Access.SuperFast {
 
 	        const string createTableQuery = @"CREATE TABLE #CompanyIds ( iconum INT NOT NULL PRIMARY KEY )";
 
-            const string pantheonQuery = @"SELECT 
-                        t2.Cusip, t2.SecPermId, t2.Value, t2.Date, t2.ItemName, t2.STDCode, t2.iconum, t2.rank
-	                FROM (
-		                SELECT 
-                            stds.SecurityID Cusip, p.PermId SecPermId, stds.Value, std.ItemName, std.STDCode, ts.TimeSliceDate Date, p.iconum iconum,
-			                row_number() over (partition by stds.STDItemID, p.PermId order by ts.TimeSliceDate desc, ts.ReportTypeID asc, ts.AutoCalcFlag ASC) as rank 
-			            FROM #CompanyIds i (nolock)
-							join DocumentSeries ds (nolock) on ds.CompanyID = i.iconum
-                            join SuperCore.MigrateToTemplates mts with (nolock) on mts.iconum = i.iconum and mts.MigrationstatusId = 1
-							join supercore.documenttimeslice d (nolock) on d.DocumentSeriesID = ds.ID
-							join Document d1 on d1.DAMDocumentId = d.DamDocumentID and d.DocumentSeriesID = d1.documentseriesid
-							join supercore.TimeSlice ts (nolock) on ts.id = d.timesliceid and ts.EncoreFlag = 0
-							join supercore.STDTimeSliceDetail stds (nolock) on stds.TimeSliceID = ts.Id
-							join STDItem std (nolock) on stds.STDItemId = std.ID and std.SecurityFlag = 1
-							join STDTemplateItem t (nolock) on t.STDItemID = std.ID and t.STDTemplateMasterCode = 'PSIT'
-							join (Select distinct cusip, PermId, h.iconum from PpiIconumMapHistory h join #CompanyIds i on h.iconum = i.iconum) p
-								on p.CUSIP = stds.SecurityID and p.iconum = ds.CompanyID and p.Iconum > 0
-			            WHERE stds.SecurityId is not null and std.STDCode =  @stdCode AND ts.TimeSliceDate <= @searchDate AND (@since IS NULL OR ts.TimeSliceDate >= @since) and d1.ExportFlag = 1
-	                ) t2
-                    ORDER BY t2.SecPermId, t2.rank
-union 
-
-SELECT 
+            // CQ 91901: Final reports (Annual & Interim) should have higher priority over Prelim reports
+            const string dcQuery =
+                @"SELECT 
                         t2.Cusip, t2.SecPermId, t2.Value, t2.Date, t2.ItemName, t2.STDCode, t2.iconum, t2.rank
 	                FROM (
 		                SELECT 
@@ -59,16 +39,11 @@ SELECT
 							join STDTemplateItem t (nolock) on t.STDItemID = std.ID and t.STDTemplateMasterCode = 'PSIT'
 							join (Select distinct cusip, PermId, h.iconum from PpiIconumMapHistory h join #CompanyIds i on h.iconum = i.iconum) p
 								on p.CUSIP = stds.SecurityID and p.iconum = ds.CompanyID and p.Iconum > 0
-				            left joinSuperCore.MigrateToTemplates mts with (nolock) on mts.iconum = i.iconum
-			            WHERE ( mts.MigrationstatusId is null or mts.MigrationstatusId != 1) and   std.STDCode =  @stdCode AND ts.TimeSliceDate <= @searchDate AND (@since IS NULL OR ts.TimeSliceDate >= @since) and d.ExportFlag = 1
+				            join MigrateToTimeSlice mi (nolock) on mi.Iconum = i.iconum and mi.MigrationStatusID = 1
+			            WHERE std.STDCode =  @stdCode AND ts.TimeSliceDate <= @searchDate AND (@since IS NULL OR ts.TimeSliceDate >= @since) and d.ExportFlag = 1
 	                ) t2
-                    ORDER BY t2.SecPermId, t2.rank
+                    ORDER BY t2.SecPermId, t2.rank";
 
-
-";
-
-            // CQ 91901: Final reports (Annual & Interim) should have higher priority over Prelim reports
-  
             var searchDate = DateTime.Now;
 	        if (reportDate != null) {
 	            searchDate = (DateTime) reportDate;
@@ -100,7 +75,7 @@ SELECT
 	            }
 
 	            // Fetch FPE data
-	            using (SqlCommand cmd = new SqlCommand(pantheonQuery, connection)) {
+	            using (SqlCommand cmd = new SqlCommand(dcQuery, connection)) {
 	                cmd.CommandTimeout = 120;
 	                cmd.Parameters.Add(new SqlParameter("@stdCode", SqlDbType.Char, 5) {Value = stdCode});
 	                cmd.Parameters.Add(new SqlParameter("@searchDate", SqlDbType.DateTime2) {Value = searchDate});
@@ -151,34 +126,9 @@ SELECT
 
             // CQ 91901: Final reports (Annual & Interim) should have higher priority over Prelim reports
             const string query =
-                @"SELECT 
-                        t2.Cusip, t2.SecPermId, t2.Value, t2.Date, t2.ItemName, t2.STDCode, t2.iconum
+                @"SELECT temp.Cusip, temp.SecPermId, temp.Value, temp.Date, temp.ItemName, temp.STDCode, temp.iconum 
 	                FROM (
-		                SELECT 
-                            stds.SecurityID Cusip, p.PermId SecPermId, stds.Value, std.ItemName, std.STDCode, ts.TimeSliceDate Date, p.iconum iconum,
-			                row_number() over (partition by stds.STDItemID, p.PermId order by ts.TimeSliceDate desc, ts.ReportTypeID asc, ts.AutoCalcFlag ASC) as rank 
-			            FROM #CompanyIds i (nolock)
-							join DocumentSeries ds (nolock) on ds.CompanyID = i.iconum
-                            join SuperCore.MigrateToTemplates mts with (nolock) on mts.iconum = i.iconum and mts.MigrationstatusId = 1
-							join supercore.documenttimeslice d (nolock) on d.DocumentSeriesID = ds.ID
-							join Document d1 on d1.DAMDocumentId = d.DamDocumentID and d.DocumentSeriesID = d1.documentseriesid
-							join supercore.TimeSlice ts (nolock) on ts.id = d.timesliceid and ts.EncoreFlag = 0
-							join supercore.STDTimeSliceDetail stds (nolock) on stds.TimeSliceID = ts.Id
-							join STDItem std (nolock) on stds.STDItemId = std.ID and std.SecurityFlag = 1
-							join STDTemplateItem t (nolock) on t.STDItemID = std.ID and t.STDTemplateMasterCode = 'PSIT'
-							join (Select distinct cusip, PermId, h.iconum from PpiIconumMapHistory h join #CompanyIds i on h.iconum = i.iconum) p
-								on p.CUSIP = stds.SecurityID and p.iconum = ds.CompanyID and p.Iconum > 0
-			            WHERE ts.TimeSliceDate <= @searchDate AND (@since IS NULL OR ts.TimeSliceDate >= @since) and d1.ExportFlag = 1
-	                ) t2
-	               WHERE t2.rank = 1 and t2.SecPermId IS NOT NULL
-                    
-union 
-
-SELECT 
-                        t2.Cusip, t2.SecPermId, t2.Value, t2.Date, t2.ItemName, t2.STDCode, t2.iconum
-	                FROM (
-		                SELECT 
-                            stds.SecurityID Cusip, p.PermId SecPermId, stds.Value, std.ItemName, std.STDCode, ts.TimeSliceDate Date, p.iconum iconum,
+		                SELECT stds.SecurityID Cusip, p.PermId SecPermId, stds.Value, std.ItemName, std.STDCode, ts.TimeSliceDate Date, p.iconum iconum,
 			                row_number() over (partition by stds.STDItemID, p.PermId order by ts.TimeSliceDate desc, ts.ReportTypeID asc, ts.AutoCalcFlag ASC) as rank 
 			            FROM #CompanyIds i (nolock)
 							join DocumentSeries ds (nolock) on ds.CompanyID = i.iconum
@@ -189,11 +139,10 @@ SELECT
 							join STDTemplateItem t (nolock) on t.STDItemID = std.ID and t.STDTemplateMasterCode = 'PSIT'
 							join (Select distinct cusip, PermId, h.iconum from PpiIconumMapHistory h join #CompanyIds i on h.iconum = i.iconum) p
 								on p.CUSIP = stds.SecurityID and p.iconum = ds.CompanyID and p.Iconum > 0
-				            left join SuperCore.MigrateToTemplates mts with (nolock) on mts.iconum = i.iconum
-			            WHERE ( mts.MigrationstatusId is null or mts.MigrationstatusId != 1) AND ts.TimeSliceDate <= @searchDate AND (@since IS NULL OR ts.TimeSliceDate >= @since) and d.ExportFlag = 1
-	                ) t2
-                   WHERE t2.rank = 1 and t2.SecPermId IS NOT NULL
-";
+				            join MigrateToTimeSlice mi (nolock) on mi.Iconum = i.iconum and mi.MigrationStatusID = 1
+			            WHERE ts.TimeSliceDate <= @searchDate AND (@since IS NULL OR ts.TimeSliceDate >= @since) and d.ExportFlag = 1
+	                ) temp
+	                WHERE temp.rank = 1 and temp.SecPermId IS NOT NULL";
 
             var searchDate = DateTime.Now;
 			if (reportDate != null) {
