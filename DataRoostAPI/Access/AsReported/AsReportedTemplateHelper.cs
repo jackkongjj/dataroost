3724bbdb91196400fbbb31d4b6c30be94cd6b96b
Lun De Jiang <ljiang@factset.com>
2018-07-17 15:26:02 -0400

    fixing CurrencyCode issue

--- DataRoostAPI/Access/AsReported/AsReportedTemplateHelper.cs
+++ DataRoostAPI/Access/AsReported/AsReportedTemplateHelper.cs
@@ -4470,7 +4470,7 @@ OUTPUT $action, 'TableCell', inserted.Id,0 INTO @ChangeResult;
 								elem["obj"]["ScalingFactorID"].AsString(),
 								elem["obj"]["AsReportedScalingFactor"].AsString(),
 								elem["obj"]["Currency"].AsString(),
-								elem["obj"]["CurrencyCode"].AsString(),
+								elem["obj"]["CurrencyCode"].AsCurrencyCode(),
 								elem["obj"]["Cusip"].AsString(),
 								"0",
 								elem["obj"]["IsIncomePositive"].AsBoolean(),
@@ -4497,7 +4497,7 @@ OUTPUT $action, 'TableCell', inserted.Id,0 INTO @ChangeResult;
 								elem["obj"]["ScalingFactorID"].AsString(),
 								elem["obj"]["AsReportedScalingFactor"].AsString(),
 								elem["obj"]["Currency"].AsString(),
-								elem["obj"]["CurrencyCode"].AsSafeString(),
+								elem["obj"]["CurrencyCode"].AsCurrencyCode(),
 								elem["obj"]["Cusip"].AsString(),
 								"0",
 								elem["obj"]["IsIncomePositive"].AsBoolean(),
@@ -4525,7 +4525,7 @@ OUTPUT $action, 'TableCell', inserted.Id,0 INTO @ChangeResult;
 								elem["obj"]["ScalingFactorID"].AsString(),
 								elem["obj"]["AsReportedScalingFactor"].AsString(),
 								elem["obj"]["Currency"].AsString(),
-								elem["obj"]["CurrencyCode"].AsString(),
+								elem["obj"]["CurrencyCode"].AsCurrencyCode(),
 								elem["obj"]["Cusip"].AsString(),
 								"0",
 								elem["obj"]["IsIncomePositive"].AsBoolean(),
@@ -4552,7 +4552,7 @@ OUTPUT $action, 'TableCell', inserted.Id,0 INTO @ChangeResult;
 								elem["obj"]["ScalingFactorID"].AsString(),
 								elem["obj"]["AsReportedScalingFactor"].AsString(),
 								elem["obj"]["Currency"].AsString(),
-							elem["obj"]["CurrencyCode"].AsSafeString(),
+							elem["obj"]["CurrencyCode"].AsCurrencyCode(),
 								elem["obj"]["Cusip"].AsString(),
 								"0",
 								elem["obj"]["IsIncomePositive"].AsBoolean(),
@@ -5345,6 +5345,7 @@ OUTPUT $action, 'DocumentTable', inserted.Id,0 INTO @ChangeResult;
 			result.ReturnValue["DebugMessage"] = "";
 			System.Text.StringBuilder sb = new System.Text.StringBuilder();
 			sb.AppendLine("SET TRANSACTION ISOLATION LEVEL SNAPSHOT;");
+			sb.AppendLine("BEGIN TRY");
 			sb.AppendLine("BEGIN TRAN");
 			sb.AppendLine("DECLARE @ChangeResult TABLE (ChangeType VARCHAR(10), TableType varchar(50), Id INTEGER, Info INTEGER)");
 
@@ -5374,6 +5375,10 @@ OUTPUT $action, 'DocumentTable', inserted.Id,0 INTO @ChangeResult;
 				sb.AppendLine("select @totalUpdate = count(*) from @ChangeResult where ChangeType = 'UPDATE'; ");
 				sb.AppendLine();
 				sb.AppendLine(string.Format("IF (@totalInsert + @totalUpdate = ({0} + {1})) BEGIN select 'commit'; COMMIT TRAN END ELSE BEGIN select 'rollback'; ROLLBACK TRAN END", totalInsert, totalUpdates));
+				sb.AppendLine("END TRY");
+				sb.AppendLine("BEGIN CATCH");
+				sb.AppendLine("select 'rollback'; ROLLBACK TRAN;");
+				sb.AppendLine("END CATCH");
 				result.ReturnValue["DebugMessage"] += sb.ToString();
 
 				//				return result;
@@ -5423,6 +5428,7 @@ OUTPUT $action, 'DocumentTable', inserted.Id,0 INTO @ChangeResult;
 			result.ReturnValue["DebugMessage"] = "";
 			System.Text.StringBuilder sb = new System.Text.StringBuilder();
 			sb.AppendLine("SET TRANSACTION ISOLATION LEVEL SNAPSHOT;");
+			sb.AppendLine("BEGIN TRY");
 			sb.AppendLine("BEGIN TRAN");
 			sb.AppendLine("DECLARE @ChangeResult TABLE (ChangeType VARCHAR(10), TableType varchar(50), Id INTEGER)");
 
@@ -5444,6 +5450,10 @@ OUTPUT $action, 'DocumentTable', inserted.Id,0 INTO @ChangeResult;
 				sb.AppendLine("select @totalUpdate = count(*) from @ChangeResult where ChangeType = 'UPDATE'; ");
 				sb.AppendLine();
 				sb.AppendLine(string.Format("IF (@totalInsert = {0} and @totalUpdate = {1}) BEGIN select 'commit'; COMMIT TRAN END ELSE BEGIN select 'rollback'; ROLLBACK TRAN END", totalInsert, totalUpdates));
+				sb.AppendLine("END TRY");
+				sb.AppendLine("BEGIN CATCH");
+				sb.AppendLine("select 'rollback'; ROLLBACK TRAN;");
+				sb.AppendLine("END CATCH");
 				result.ReturnValue["DebugMessage"] += sb.ToString();
 
 				//				return result;
@@ -5609,6 +5619,7 @@ where dtc.TableCellID = @id
 			result.ReturnValue["DebugMessage"] = "";
 			System.Text.StringBuilder sb = new System.Text.StringBuilder();
 			sb.AppendLine("SET TRANSACTION ISOLATION LEVEL SNAPSHOT;");
+			sb.AppendLine("BEGIN TRY");
 			sb.AppendLine("BEGIN TRAN");
 			sb.AppendLine("DECLARE @ChangeResult TABLE (ChangeType VARCHAR(10), TableType varchar(50), Id INTEGER, Info INTEGER)");
 
@@ -5624,6 +5635,10 @@ where dtc.TableCellID = @id
 				sb.AppendLine("select @totalUpdate = count(*) from @ChangeResult where ChangeType = 'UPDATE'; ");
 				sb.AppendLine();
 				sb.AppendLine(string.Format("IF (@totalInsert = {0} and @totalUpdate = {1}) BEGIN select 'commit'; COMMIT TRAN END ELSE BEGIN select 'rollback'; ROLLBACK TRAN END", totalInsert, totalUpdates));
+				sb.AppendLine("END TRY");
+				sb.AppendLine("BEGIN CATCH");
+				sb.AppendLine("select 'rollback'; ROLLBACK TRAN;");
+				sb.AppendLine("END CATCH");
 				result.ReturnValue["DebugMessage"] += sb.ToString();
 
 				//				return result;
@@ -8116,6 +8131,16 @@ INSERT [dbo].[LogAutoStitchingAgent] (
 				result = "NULL";
 			} else if (string.Equals(jString, "true", StringComparison.InvariantCultureIgnoreCase)) {
 				result = "1";
+			}
+			return result;
+		}
+		public static string AsCurrencyCode(this JToken jValue) {
+			string jString = jValue.ToString();
+			string result = "NULL";
+			if (string.Equals(jString, "null", StringComparison.InvariantCultureIgnoreCase) || string.IsNullOrWhiteSpace(jString)) {
+				result = "NULL";
+			} else {
+				result = "'" + jString.Replace("'", "''").Replace("\\\\", "\\") + "'";
 			}
 			return result;
 		}
