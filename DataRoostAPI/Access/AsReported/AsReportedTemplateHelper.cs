a16c422de7107abf7819c6163f780066c4c4cc8e
lchang <lchang@factset.com>
2019-01-08 14:42:15 -0500

    remove using MaxAdjustedOrder

--- DataRoostAPI/Access/AsReported/AsReportedTemplateHelper.cs
+++ DataRoostAPI/Access/AsReported/AsReportedTemplateHelper.cs
@@ -5813,8 +5813,8 @@ OUTPUT $action, 'DocumentTimeSlice', inserted.Id,0 INTO @ChangeResult;
 DELETE FROM dbo.StaticHierarchy where id in ({0});
 ";
 			string merge_sql = @"MERGE dbo.StaticHierarchy
-USING ( select {0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}, {10}, {11} ,ISNULL(max(AdjustedOrder), -1)  from dbo.StaticHierarchy where tabletypeID = {3} ) as src (Id,CompanyFinancialTermId,AdjustedOrder,TableTypeId
-,Description,HierarchyTypeId,SeperatorFlag,StaticHierarchyMetaId,UnitTypeId,IsIncomePositive,ChildrenExpandDown,ParentID,MaxAdjustedOrder)
+USING ( select {0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}, {10}, {11} from dbo.StaticHierarchy where tabletypeID = {3} ) as src (Id,CompanyFinancialTermId,AdjustedOrder,TableTypeId
+,Description,HierarchyTypeId,SeperatorFlag,StaticHierarchyMetaId,UnitTypeId,IsIncomePositive,ChildrenExpandDown,ParentID)
 ON dbo.StaticHierarchy.Id = src.Id
 WHEN MATCHED THEN
 	UPDATE SET CompanyFinancialTermId = src.CompanyFinancialTermId
