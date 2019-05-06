b158ab13359e556c04cdd0edf772b05f38a20b26
Lun De Jiang <ljiang@factset.com>
2019-05-06 15:46:01 -0400

    commit code for connecting to PostGres

---
+++ DataRoostAPI/Access/AsReported/VisualStitchingHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using Npgsql;
+using NpgsqlTypes;
+
+namespace CCS.Fundamentals.DataRoostAPI.Access.AsReported
+{
+    public class VisualStitchingHelper
+    {
+
+        private readonly string _sfConnectionString;
+
+        static VisualStitchingHelper()
+        {
+
+        }
+
+        public VisualStitchingHelper(string sfConnectionString)
+        {
+            this._sfConnectionString = sfConnectionString;
+        }
+
+        private string connString = "Host=ip-172-31-81-210.manager.factset.io;Port=32791;Username=uyQKYrcTSrnnqB;Password=NoCLf_xBeXiB0UXZjhZUNg7Zx8;Database=di8UFb70sJdA5e;sslmode=Require;Trust Server Certificate=true;";
+
+        public string GetJson()
+        {
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            using (
+                var conn = new NpgsqlConnection(connString))
+            {
+                conn.Open();
+
+                // Retrieve all rows
+                using (var cmd = new NpgsqlCommand("SELECT value FROM json LIMIT 1", conn))
+                using (var reader = cmd.ExecuteReader())
+                    while (reader.Read())
+                        sb.Append(reader.GetString(0));
+            }
+            return sb.ToString();
+        }
+    }
+}
